743c41dfdc881fa84197f2e3ab1935428f8c0c6a
Ephrem Demelash <demelashephrem@gmail.com>
2020-09-02 23:36:41 +0300

    adds recursive helper functions, - findMin method -findMax method

--- tree/binaryTree.js
+++ tree/binaryTree.js
@@ -1,6 +1,6 @@
 class BinaryTree {
   constructor() {
-    this.node = {
+    this._node = {
       left: null,
       right: null,
       data: null,
@@ -17,55 +17,83 @@ class BinaryTree {
     this.print(root.right);
   }
 
-  insert(data, root) {
-    if (root.data == null) {
-      root.data = data;
-      return;
-    } else {
-      if (data > root.data) {
-        if (root.right == null) {
-          let newNode = {
-            left: null,
-            right: null,
-            data: data,
-          };
-          root.right = newNode;
-          return root;
-        } else this.insert(data, root.right);
-      } else if (data < root.data) {
-        if (root.left == null) {
-          let newNode = {
-            left: null,
-            right: null,
-            data: data,
-          };
-          root.left = newNode;
-          return root;
-        } else this.insert(data, root.left);
+  insert(data) {
+    insertHelper(data, this._node);
+    function insertHelper(data, root) {
+      if (root.data == null) {
+        root.data = data;
+        return;
+      } else {
+        if (data > root.data) {
+          if (root.right == null) {
+            let newNode = {
+              left: null,
+              right: null,
+              data: data,
+            };
+            root.right = newNode;
+            return root;
+          } else insertHelper(data, root.right);
+        } else if (data < root.data) {
+          if (root.left == null) {
+            let newNode = {
+              left: null,
+              right: null,
+              data: data,
+            };
+            root.left = newNode;
+            return root;
+          } else insertHelper(data, root.left);
+        }
       }
     }
   }
 
   search(data, root) {
-    if (data == root.data) {
-      return data;
+    return serachHelper(data, this._node);
+    function serachHelper(data, root) {
+      if (data == root.data) {
+        return data;
+      }
+      if (data < root.data) {
+        return serachHelper(data, root.left);
+      } else if (data > root.data) {
+        return serachHelper(data, root.right);
+      } else return null;
     }
-    if (data < root.data) {
-     return this.search(data, root.left);
-    } else if (data > root.data) {
-      return this.search(data, root.right);
-    }else return null;
+  }
+
+  findMin() {
+    return findMinRec(this._node);
+
+    function findMinRec(root) {
+      if (root.left == null) {
+        return root.data;
+      }
+      return findMinRec(root.left);
+    }
+  }
+
+  findMax() {
+    return findMaxRec(this._node);
+
+    function findMaxRec(root) {
+      if (root.left == null) {
+        return root.data;
+      }
+      return findMaxRec(root.left);
+    }
   }
 }
 
 let bst = new BinaryTree();
-bst.insert(5, bst.node);
-bst.insert(4, bst.node);
-bst.insert(9, bst.node);
-bst.insert(6, bst.node);
-bst.insert(3, bst.node);
-bst.insert(8, bst.node);
-console.log(bst.node);
-console.log(bst.search(8, bst.node));
+bst.insert(5);
+bst.insert(4);
+bst.insert(9);
+bst.insert(6);
+bst.insert(3);
+bst.insert(8);
+console.log(bst.search(8));
+console.log(bst.findMin());
+console.log(bst.findMax());
 
-// bt.print(bt.node);
