4c3cf5d5cac8b4733782789ae22ad0965df9efa7
Ephrem Demelash <demelashephrem@gmail.com>
2020-09-02 23:06:59 +0300

    imp: binary tree.
    implements BST with inorder traversal

---
+++ tree/binaryTree.js
@@ -0,0 +1,57 @@
+class BinaryTree {
+    constructor() {
+      this.node = {
+        left: null,
+        right: null,
+        data: null,
+      };
+    }
+  
+    print(root) {
+      if (root == null) {
+        return;
+      }
+  
+      console.log(root.data)
+      this.print(root.left);
+      this.print(root.right);
+    }
+  
+    insert(data, root) {
+      if (root.data == null) {
+        root.data = data;
+        return;
+      } else {
+        if (data > root.data) {
+          if (root.right == null) {
+            let newNode = {
+              left: null,
+              right: null,
+              data: data,
+            };
+            root.right = newNode;
+            return root;
+          } else this.insert(data, root.right);
+        } else if (data < root.data) {
+          if (root.left == null) {
+            let newNode = {
+              left: null,
+              right: null,
+              data: data,
+            };
+            root.left = newNode;
+            return root;
+          } else this.insert(data, root.left);
+        }
+      }
+    }
+  }
+  
+  let bt = new BinaryTree();
+  bt.insert(5, bt.node);
+  bt.insert(4, bt.node);
+  bt.insert(9, bt.node);
+  bt.insert(6, bt.node);
+  console.log(bt.node);
+  
+  bt.print(bt.node)
