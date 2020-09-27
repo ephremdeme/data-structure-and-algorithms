83e16cca5a3c4b6e062f5345c58f6d7d64b15d9d
Ephrem Demelash <demelashephrem@gmail.com>
2020-09-27 12:18:02 +0300

    📦 NEW:  Imp reverse number problem

---
+++ problems/reverseNumber.js
@@ -0,0 +1,12 @@
+function reverseNumber(num = 0) {
+  var ans = 0;
+  while (num > 0) {
+    var remainder = num % 10;
+    ans = ans * 10 + remainder;
+    num = Math.floor(num / 10);
+  }
+
+  return ans;
+}
+
+console.log(reverseNumber(123456));
