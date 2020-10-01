e92ffeee35c9f0bfc835868ebe8132a4dccb43cc
raww-man <72182450+raww-man@users.noreply.github.com>
2020-10-01 11:15:58 +0530

    Added Stack.py
    Implementation of Stacks [In Python]

---
+++ StackImp.py
@@ -0,0 +1,26 @@
+ 
+stack = [] 
+
+# append() function to push 
+# element in the stack 
+stack.append('a') 
+stack.append('b') 
+stack.append('c') 
+
+print('Initial stack') 
+print(stack) 
+
+# pop() fucntion to pop 
+# element from stack in 
+# LIFO order 
+print('\nElements poped from stack:') 
+print(stack.pop()) 
+print(stack.pop()) 
+print(stack.pop()) 
+
+print('\nStack after elements are poped:') 
+print(stack) 
+
+# uncommenting print(stack.pop()) 
+# will cause an IndexError 
+# as the stack is now empty 
