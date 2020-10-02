5f7709b6efaead588718f8450607f298c5413ce0
adgai19 <gaitondeaditya@gmail.com>
2020-10-02 23:20:46 +0530

    Added comments and removed extra print Statements

--- Algorithms/Pascal/pascals.lua
+++ Algorithms/Pascal/pascals.lua
@@ -1,9 +1,10 @@
+-- Problem : To print Pascal's Triangle
+
+
 local n = io.read("*n")
 local c
---print(n)
 for l = 1,n do
   c=1
-  --print(c)
   for x = 1,l do
     io.write(c," ")
     c=(c*(l-x)/x)
