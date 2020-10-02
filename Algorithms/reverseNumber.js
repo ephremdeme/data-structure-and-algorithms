f5079ee9e5f9dbea3a8f83cf467b10f005c95918
Sirawit <sirawit.aarm@gmail.com>
2020-10-02 18:31:02 +0700

    reverse number with js
    - add test case

--- Algorithms/reverseNumber.js
+++ Algorithms/reverseNumber.js
@@ -2,4 +2,6 @@ function reversedNumber(num) {
     return (parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num))
 }
 
-console.log(reversedNumber(123456))
+
+console.log(reversedNumber(123456)) // 654321
+console.log(reversedNumber(-54321)) // -12345
