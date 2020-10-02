d7cec259f8f1bc9ac9f8b666f2a6cd4877eb3660
[name] <55917516+AnshMishra2001@users.noreply.github.com>
2020-10-02 21:37:36 +0530

    Update LinearSearch.java

--- searching/LinearSearch.java
+++ searching/LinearSearch.java
@@ -1,15 +1,23 @@
-public class LinearSearch{    
-public static int linearSearch(int[] arr, int key){    
-        for(int i=0;i<arr.length;i++){    
-            if(arr[i] == key){    
+import java.util.Scanner;
+
+public class linearSearch{    
+    static int linearSearch(int[] arr, int key){    
+        for(int i=0;i<arr.length;i++)    
+            if(arr[i] == key)    
                 return i;    
-            }    
-        }    
         return -1;    
     }    
     public static void main(String args[]){    
-        int[] a= {10,20,30,50,70,90};    
-        int key = 50;    
-        System.out.println(key+" is found at an index: "+linearSearch(a, key));    
+        System.out.print("Enter the no: of elements in the array: ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[50];
+        System.out.print("Enter the elements:\n");
+        for(int i = 0; i < n; i++)
+            arr[i] = scan.nextInt();
+        System.out.print("Enter the key: ");
+        int key = scan.nextInt();
+        scan.close();
+        System.out.println(key+" found at an index: "+ linearSearch(arr, key));    
     }    
 }   
