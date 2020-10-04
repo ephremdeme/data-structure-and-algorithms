32fa5a903a49b76d7c0f32fd38b22e48435f01a6
AMAR SHANKAR <amarks444@gmail.com>
2020-10-04 12:34:06 +0530

    Updated DijkstrasUsingSetInSTL for implicits Graphs..

--- Algorithms/DijkstrasUsingSetInSTL.cpp
+++ Algorithms/DijkstrasUsingSetInSTL.cpp
@@ -76,24 +76,38 @@ void Graph::shortestPath(int src)
 int main() 
 { 
 	
-	int V = 9; 
-	Graph g(V); 
-
+// 	int V = 9; 
+// 	Graph g(V); 
 	
-	g.addEdge(0, 1, 4); 
-	g.addEdge(0, 7, 8); 
-	g.addEdge(1, 2, 8); 
-	g.addEdge(1, 7, 11); 
-	g.addEdge(2, 3, 7); 
-	g.addEdge(2, 8, 2); 
-	g.addEdge(2, 5, 4); 
-	g.addEdge(3, 4, 9); 
-	g.addEdge(3, 5, 14); 
-	g.addEdge(4, 5, 10); 
-	g.addEdge(5, 6, 2); 
-	g.addEdge(6, 7, 1); 
-	g.addEdge(6, 8, 6); 
-	g.addEdge(7, 8, 7); 
+        int TotalNodes,TotalEdges;
+	cout<<"Enter Total Nodes: "<<"\n";
+	cin>>TotalNodes;
+	cout<<"Enter Total Edges: "<<"\n";
+	cin>>TotalEdges;
+	
+	Graph g(TotalNodes); 
+	
+	for(int i=0;i<TotalEdges;i++)
+	{
+		int u,v,w;
+		cin>>u>>v>>w;
+		g.addEdge(u,v,w);
+	}
+	
+// 	g.addEdge(0, 1, 4); 
+// 	g.addEdge(0, 7, 8); 
+// 	g.addEdge(1, 2, 8); 
+// 	g.addEdge(1, 7, 11); 
+// 	g.addEdge(2, 3, 7); 
+// 	g.addEdge(2, 8, 2); 
+// 	g.addEdge(2, 5, 4); 
+// 	g.addEdge(3, 4, 9); 
+// 	g.addEdge(3, 5, 14); 
+// 	g.addEdge(4, 5, 10); 
+// 	g.addEdge(5, 6, 2); 
+// 	g.addEdge(6, 7, 1); 
+// 	g.addEdge(6, 8, 6); 
+// 	g.addEdge(7, 8, 7); 
 
 	g.shortestPath(0); 
 
