c7a31df44f72d308f87377ec42e2490f4e76c147
Phil Stavridis <phizmo@gmail.com>
2020-10-25 17:47:14 +0000

    Added functionaliy for int

--- problems/palindrome/palindrome.py
+++ problems/palindrome/palindrome.py
@@ -1,4 +1,5 @@
 def is_palindrome(s):
+    s = str(s)
     rev = s[::-1]
     if s == rev:
         return True
@@ -6,6 +7,6 @@ def is_palindrome(s):
 
 
 if __name__ == '__main__':
-    inputs = ['orange', 'minim']
+    inputs = ['orange', 'minim',  12321]
     for i in inputs:
         print(f'{i} is a palindrome:{is_palindrome(i)}')
