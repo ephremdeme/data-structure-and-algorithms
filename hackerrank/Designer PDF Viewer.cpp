145d08271010c2d0e80c380ca36836628d691675
Amar Shankar <amarks444@gmail.com>
2021-10-26 15:27:10 +0530

    hackerrank Solved - Designer PDF Viewer

---
+++ hackerrank/Designer PDF Viewer.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+int designerPdfViewer(vector<int> h, string word) {
+    vector<int> ans;
+    for( int i =0;i<word.length();i++){
+        ans.push_back(h[word[i] - 'a']);
+    }
+    return (*max_element(ans.begin(),ans.end()))*(ans.size());
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split(rtrim(h_temp_temp));
+
+    vector<int> h(26);
+
+    for (int i = 0; i < 26; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    string word;
+    getline(cin, word);
+
+    int result = designerPdfViewer(h, word);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
